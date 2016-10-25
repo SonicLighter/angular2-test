eda89b1ca5bea64f2eaf0584be7effa8b65ef643
SonicLighter <pashkevich.s.d@yandex.ru>
2016-10-25 17:54:03 +0300

    show todo list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,25 @@
 import {Component} from '@angular/core';
 
 @Component({
+    /*
     selector: 'todo-app',
     templateUrl: './app/app.component.html',
     styleUrls: ['./app/app.component.css']
+    */
+    selector: 'todo-app',
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css'],
 })
 export class AppComponent {
     title: string;
+    taskList: string[];
 
     constructor() {
-        this.title = 'Angular 2Do!';
+        this.title = 'ANGULAR2';
+        this.taskList = [/*'Learn JavaScript', 'Learn ES6', 'Learn Angular2', 'Learn something'*/];
+    }
+
+    addTask() {
+        
     }
 }
